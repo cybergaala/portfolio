b4cb05911fe4fd6d293cfb2cac7d6be3c5a2e431
Edgar Mejía <edgar13155@gmail.com>
2025-07-20 00:09:01 -0600

    Added base plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap(), mdx(), preact({ compat: true })],
+});
