8beb3ff0e74b51c34bbd6540ec2da6d9dc27f147
Edgar Mejía <edgar13155@gmail.com>
2026-05-02 00:03:35 -0600

    Added lazy load support for blog images

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,19 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 
+function rehypeLazyImages() {
+  return (tree) => {
+    function walk(node) {
+      if (node.type === 'element' && node.tagName === 'img') {
+        node.properties = node.properties ?? {};
+        if (!node.properties.loading) node.properties.loading = 'lazy';
+      }
+      if (node.children) node.children.forEach(walk);
+    }
+    walk(tree);
+  };
+}
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://edgarmejia.com',
@@ -19,5 +32,5 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-  integrations: [sitemap(), mdx(), preact({ compat: true })],
+  integrations: [sitemap(), mdx({ rehypePlugins: [rehypeLazyImages] }), preact({ compat: true })],
 });
